cf1d8c1bb79dd81f236e10c28e9fabdb0dc2e379
tylerlisk <tylerlisk34@gmail.com>
2025-03-20 01:49:13 -0500

    configured local sqlite db
    using local database to test app routes before deploying to aws

--- habit_app/__init__.py
+++ habit_app/__init__.py
@@ -2,10 +2,19 @@ from flask import Flask
 from .config import Config
 from .extensions import login_manager, db, bcrypt
 from . import auth, habit
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    INSTANCE_DIR = os.path.join(BASE_DIR, "..", "instance") 
+    os.makedirs(INSTANCE_DIR, exist_ok=True)
+     
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(INSTANCE_DIR, 'habit_database.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -13,6 +22,11 @@ def create_app():
 
     app.register_blueprint(auth.bp, url_prefix = "/auth")
     app.register_blueprint(habit.bp)
+
+
+    with app.app_context():
+        from . import models
+        db.create_all()
 
     return app
 
