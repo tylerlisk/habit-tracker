a4363ecc354ced3732daa539a971923d352efec6
tylerlisk <tylerlisk34@gmail.com>
2025-03-20 00:59:29 -0500

    Created db models for user and habits

---
+++ habit_app/models.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from .extensions import db
+from flask_login import UserMixin
+from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey, Enum as SQLAlchemyEnum, func
+from sqlalchemy.orm import relationship
+
+class Categories(Enum):
+    CREATIVE = "Creative"
+    HEALTH = "Health"
+    FITNESS = "Fitness"
+    PRODUCTIVITY = "Productivity"
+    FINANCIAL = "Financial"
+
+    def __str__(self):
+        return self.value 
+
+class HabitUser(db.Model, UserMixin):  
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(150), unique=True, nullable=False)
+    password = Column(String(150), nullable=False)
+
+    habits = relationship("Habit", back_populates="user", cascade="all, delete-orphan")
+
+class Habit(db.Model):
+    __tablename__ = "habits"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    category = Column(SQLAlchemyEnum(Categories), nullable=False) 
+    created = Column(Date, default=func.current_date())
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    user = relationship("HabitUser", back_populates="habits") 
+
+    def __repr__(self):
+        return f"<Habit {self.name}, Category: {self.category.value}>"
